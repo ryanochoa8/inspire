87c5ba20c95245f48f77a1f815450d8468825a36
ryanochoa8 <ryan.ochoa888@gmail.com>
2019-06-28 13:05:06 -0600

    changes

--- app/models/todo.js
+++ app/models/todo.js
@@ -8,18 +8,14 @@ export default class Todo {
   get Template() {
     let template = `
       <div class="col-4">
-        <p><b>Description:</b> ${this.description}</p>
-        <span><input class="form-check-input inline" type="checkbox" value="" onclick="app.controllers.todoController.toggleTodoStatus('${this._id}')">Completed</span>
-        
+      <input class="form-check-input inline" type="checkbox" value="" onclick="app.controllers.todoController.toggleTodoStatus('${this._id}')"><p><b>Description:</b> ${this.description}</p>
       </div>
         `
     if (this.completed == true) {
       template = `
       <div class="col-4">
-        <p><b>Description:</b> ${this.description}</p>
-        <span><input class="form-check-input inline" type="checkbox" value="" onclick="app.controllers.todoController.toggleTodoStatus('${this._id}')">Undo Completion</span>
-
-        <button type="delete" class="btn btn-danger" onclick="app.controllers.todoController.removeTodo('${this._id}')">Delete Todo</button>
+      <input class="form-check-input inline" type="checkbox" value="" onclick="app.controllers.todoController.toggleTodoStatus('${this._id}')" checked><strike><p><b>Description:</b> ${this.description}</p></strike>
+        <button type="delete" class="btn btn-danger btn-sm" onclick="app.controllers.todoController.removeTodo('${this._id}')">Delete Todo</button>
       </div>
       `
     }
